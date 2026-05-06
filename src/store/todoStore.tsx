bbaeeb2a1b07e0e654df95985acda3ce288b0ab0
mohitpuri-goswami <mohitpuri.goswami@simformsolutions.com>
2026-05-06 10:45:53 +0530

    feat: Add immer for updating the todos

--- src/store/todoStore.tsx
+++ src/store/todoStore.tsx
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
 
 interface Todo {
   id: number;
@@ -11,48 +12,45 @@ interface TodoStore {
   todos: Todo[];
 }
 
-export const useTodoStore = create<TodoStore>(() => ({
-  todos: [],
-}));
+export const useTodoStore = create<TodoStore>()(
+  immer(() => ({
+    todos: [],
+  })),
+);
 
 export const addTodo = (task: string) => {
-  useTodoStore.setState((state) => ({
-    todos: [
-      ...state.todos,
-      {
-        id: state.todos.length + 1,
-        task,
-        createdAt: new Date(),
-        isDone: false,
-      },
-    ],
-  }));
+  useTodoStore.setState((state) => {
+    state.todos.push({
+      id: state.todos.length + 1,
+      task,
+      createdAt: new Date(),
+      isDone: false,
+    });
+  });
 };
 
 export const deleteTodo = (id: number) => {
-  useTodoStore.setState((state) => ({
-    todos: state.todos.filter((todo) => todo.id !== id),
-  }));
+  useTodoStore.setState((state) => {
+    state.todos = state.todos.filter((todo) => todo.id !== id);
+  });
 };
 
 export const markAsDone = (id: number) => {
-  useTodoStore.setState((state) => ({
-    todos: state.todos.map((todo) =>
-      todo.id === id ? { ...todo, isDone: true } : todo,
-    ),
-  }));
+  useTodoStore.setState((state) => {
+    const todo = state.todos.find((todo) => todo.id === id);
+    if (todo) todo.isDone = true;
+  });
 };
 
 export const editTodo = (id: number, task: string) => {
-  useTodoStore.setState((state) => ({
-    todos: state.todos.map((todo) =>
-      todo.id === id ? { ...todo, task } : todo,
-    ),
-  }));
+  useTodoStore.setState((state) => {
+    const todo = state.todos.find((todo) => todo.id === id);
+    if (todo) todo.task = task;
+  });
 };
 
 export const filterByTitle = (task: string) => {
-  useTodoStore.setState((state) => ({
-    todos: state.todos.filter((todo) => todo.task === task),
-  }));
+  useTodoStore.setState((state) => {
+    state.todos = state.todos.filter((todo) => todo.task === task);
+  });
 };
